583c359d82ad2a77b83a2119c88a97b1ea5c4efb
mathxs <matheusdsantosp@gmail.com>
2019-07-27 15:14:28 -0300

    Testando o ambiente Virtual,e iniciando o projeto

---
+++ Projeto/main.py
@@ -0,0 +1,7 @@
+#Artigo referencia do trabalho de PLN
+#https://pdfs.semanticscholar.org/7867/159013a9c10661fbfe8619a9c1cc76c3012c.pdf?_ga=2.107625477.667567004.1564233162-1809408230.1564233162
+#
+#Instrução do Virtual Env: http://libzx.so/main/learning/2016/03/13/best-practice-for-virtualenv-and-git-repos.html
+#https://pt.stackoverflow.com/questions/391796/devo-commitar-meu-virtualenv-no-github
+
+print("Testando Ambiente Virtual e Compilador")
